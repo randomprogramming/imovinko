f6ff65de5d6173cb6f9840da52f2f48774753157
randomprogramming <dgoluban38@gmail.com>
2023-09-20 17:15:15 +0200

    fixed scrolling in modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -40,11 +40,13 @@ export default function Modal({ show, children, onClose, small }: ModalProps) {
             <div
                 className={`bg-zinc-100 z-50 fixed overflow-auto flex ${
                     small
-                        ? "rounded w-fit h-fit max-h-[80vh] overflow-y-auto"
+                        ? "rounded w-fit h-fit max-h-[80vh]"
                         : "rounded-xl left-2 right-2 top-2 bottom-2 md:left-10 md:right-10 md:top-10 md:bottom-10 lg:left-32 lg:right-32 lg:top-32 lg:bottom-32"
                 } `}
             >
-                {show && children}
+                <div className="h-full max-h-full overflow-y-auto flex flex-col">
+                    {show && children}
+                </div>
             </div>
         </div>
     );
